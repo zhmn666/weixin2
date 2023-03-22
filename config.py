c71864f7e2449e629d12834073a5228b99f15ecc
zhmn666 <128480158+zhmn666@users.noreply.github.com>
2023-03-22 19:38:59 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+# 公众号配置
+# 公众号appId
+app_id = "wxfbc085e141ffffde"
+# 公众号appSecret
+app_secret = "3d48dd651016b17a3910143f750e05d6"
+# 模板消息id
+template_id = "pYWNqTLTvBu-7Wt_KMyHlJRZBCuFTw6fGFLyszVFDHM"
+# 这是openid
+user = ["onjZG6LrWLvEA5MFG_DZQ4IEAtWw"]
+
+# 信息配置
+# 所在省份
+province = "浙江"
+# 所在城市
+city = "杭州"
+# 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
+birthday = "2001-1-1"
+# 在一起的日子，格式同上
+love_date = "2001-1-1"
+
