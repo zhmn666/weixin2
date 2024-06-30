1c60124b9e3b4dfe3f634063b8362c77f27e2e55
张勐 <128480158+zhmn666@users.noreply.github.com>
2024-06-30 21:08:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ app_id = "wxc8fbd71e30c10202"
 # 公众号appSecret
 app_secret = "10202fa3a93f77fd697efc190ab6d1c8"
 # 模板消息id
-template_id = "cUdlRwNdH215ViKrWx1j3AR9yeFSxpkLMDeudA6VKbY"
+template_id = "8hQat2ETvTIT72WKb2i1aV7cdLVH-z4K6Vx5l2tCbJQ"
 # 这是openid
-user = ["oFaxG6QX4Wl9TtCHUpprci6ZzFPk"]
+user = ["oFaxG6QX4Wl9TtCHUpprci6ZzFPk","oFaxG6Xy4RN12j6m94zvkABhrFVg","oFaxG6c2zV6mdSv_IycyqpHUZlf0"]
 
 # 信息配置
 # 所在省份
